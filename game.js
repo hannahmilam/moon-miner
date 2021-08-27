845038dc9c706a9f8f008d8c4774dfbd8344d5f0
Hannah Milam <hannah.milam16@gmail.com>
2021-08-27 15:14:36 -0600

    layout

--- game.js
+++ game.js
@@ -1,5 +1,67 @@
+// VARIABLES :)
 let cheese = 0
+let clickUpgrades = {
+shovel: {
+  price: 100,
+  quantity: 0,
+  multiplier: 1
+},
+dynamite: {
+  price: 500,
+  quantity: 0,
+  multiplier: 5
+}
+};
 
+let autoUpgrades = {
+  rover: {
+    price: 250,
+    quantity: 0,
+    multiplier: 10
+  },
+  alien: {
+    price: 500,
+    quantity: 0,
+    multiplier: 20
+  }
+  };
+
+  // FUNCTIONS :)
 function mine(){
-    
+  let currentCheese = cheese++
+   console.log(cheese)
+   update()
+   return (currentCheese)
+}
+
+
+function drawClickUpgrades(){
+  for(let key in clickUpgrades){
+    console.log(key)
+    let item = clickUpgrades[key]
+  }
+  
+}
+
+function drawAutoUpgrades(){
+  for(let key in autoUpgrades){
+    console.log(key)
+    let item = autoUpgrades[key]
+  }
+}
+
+function update(){
+  document.getElementById("current-cheese").innerHTML = cheese
+  document.getElementById("current-shovel").innerHTML = clickUpgrades.shovel.quantity
+  document.getElementById("current-dynamite").innerHTML = clickUpgrades.dynamite.quantity
+  document.getElementById("current-rover").innerHTML = autoUpgrades.rover.quantity
+  document.getElementById("current-alien").innerHTML = autoUpgrades.alien.quantity
+}
+
+function startInterval(){
+  collectionInterval = setInterval(collectAutoUpgrades, 3000);
+}
+
+function collectAutoUpgrades(){
+
 }

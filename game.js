4361fec997af93023b635df7f417ac631d4bd6a1
Hannah Milam <hannah.milam16@gmail.com>
2021-08-27 16:25:51 -0600

    updated upgrade count

--- game.js
+++ game.js
@@ -2,12 +2,12 @@
 let cheese = 0
 let clickUpgrades = {
 shovel: {
-  price: 100,
+  price: 5,
   quantity: 0,
   multiplier: 1
 },
 dynamite: {
-  price: 500,
+  price: 10,
   quantity: 0,
   multiplier: 5
 }
@@ -15,12 +15,12 @@ dynamite: {
 
 let autoUpgrades = {
   rover: {
-    price: 250,
+    price: 10,
     quantity: 0,
     multiplier: 10
   },
   alien: {
-    price: 500,
+    price: 25,
     quantity: 0,
     multiplier: 20
   }
@@ -31,23 +31,32 @@ function mine(){
   let currentCheese = cheese++
    console.log(cheese)
    update()
-   return (currentCheese)
 }
 
+function buyShovel(){
+  let shovel = clickUpgrades.shovel.quantity ++
+  console.log(shovel)
+  update()
 
-function drawClickUpgrades(){
-  for(let key in clickUpgrades){
-    console.log(key)
-    let item = clickUpgrades[key]
-  }
-  
+}
+function buyDynamite(){
+  let dynamite = clickUpgrades.dynamite.quantity ++
+  console.log(dynamite)
+  update()
+
 }
 
-function drawAutoUpgrades(){
-  for(let key in autoUpgrades){
-    console.log(key)
-    let item = autoUpgrades[key]
-  }
+function buyRover(){
+  let rover = autoUpgrades.rover.quantity ++
+  console.log(rover)
+  update()
+
+}
+function buyAlien(){
+  let alien = autoUpgrades.alien.quantity ++
+  console.log(alien)
+  update()
+
 }
 
 function update(){
@@ -56,12 +65,5 @@ function update(){
   document.getElementById("current-dynamite").innerHTML = clickUpgrades.dynamite.quantity
   document.getElementById("current-rover").innerHTML = autoUpgrades.rover.quantity
   document.getElementById("current-alien").innerHTML = autoUpgrades.alien.quantity
-}
-
-function startInterval(){
-  collectionInterval = setInterval(collectAutoUpgrades, 3000);
-}
-
-function collectAutoUpgrades(){
 
 }

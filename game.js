dbf30111083c7f7d473adac2c793bf99ee8ee6c7
Hannah Milam <hannah.milam16@gmail.com>
2021-08-28 12:05:56 -0600

    updated theme

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 // VARIABLES :)
-let cheese = 0
+let spaceRock = 0
 let clickUpgrades = {
-shovel: {
+sword: {
   price: 5,
   quantity: 0,
   multiplier: 1
 },
-dynamite: {
+portalCutter: {
   price: 10,
   quantity: 0,
   multiplier: 5
@@ -14,12 +14,12 @@ dynamite: {
 };
 
 let autoUpgrades = {
-  rover: {
+  plasmaGun: {
     price: 10,
     quantity: 0,
     multiplier: 10
   },
-  alien: {
+  explosive: {
     price: 25,
     quantity: 0,
     multiplier: 20
@@ -28,65 +28,84 @@ let autoUpgrades = {
 
   // FUNCTIONS :)
 function mine(){
-   cheese++
+  spaceRock++
    update()
 }
-function buyShovel(){
-  let price = clickUpgrades.shovel.price;
-  if(cheese >= price) {
-  cheese -= price;
-  clickUpgrades.shovel.quantity++;
-  clickUpgrades.shovel.price += Math.floor(price * 0.10);
+// NOTE this section is buyUpgrades section. Need to make this re-usable after requirements are completed.
+function buySword(){
+  let price = clickUpgrades.sword.price;
+  if(spaceRock >= price) {
+  spaceRock -= price;
+  clickUpgrades.sword.quantity++;
+  clickUpgrades.sword.price += Math.floor(price * 0.30);
   }else {
-  console.log("not enough cheese!")
+  console.log("not enough space rock!")
   }
-  document.getElementById("currentShovel").innerText = clickUpgrades.shovel.quantity.toString();
-  document.getElementById('shovelPrice').innerText = clickUpgrades.shovel.price.toString();
+  document.getElementById("currentSword").innerText = clickUpgrades.sword.quantity.toString();
+  document.getElementById('swordPrice').innerText = clickUpgrades.sword.price.toString();
   update();
   }
 
-function buyDynamite(){
-  let price = clickUpgrades.dynamite.price;
-  if(cheese >= price) {
-  cheese -= price;
-  clickUpgrades.dynamite.quantity++;
-  clickUpgrades.dynamite.price += Math.floor(price * 0.10);
-  }else if(cheese < price) {
-  console.log("not enough cheese!")
+function buyPortalCutter(){
+  let price = clickUpgrades.portalCutter.price;
+  if(spaceRock >= price) {
+  spaceRock -= price;
+  clickUpgrades.portalCutter.quantity++;
+  clickUpgrades.portalCutter.price += Math.floor(price * 0.30);
+  }else if(spaceRock < price) {
+  console.log("not enough space rock!")
   }
-  document.getElementById("currentDynamite").innerText = clickUpgrades.dynamite.quantity.toString();
-  document.getElementById('dynamitePrice').innerText = clickUpgrades.dynamite.price.toString();
+  document.getElementById("currentPortalCutter").innerText = clickUpgrades.portalCutter.quantity.toString();
+  document.getElementById('portalCutterPrice').innerText = clickUpgrades.dynamite.price.toString();
   update();
   }
 
-  function buyRover(){
-    let price = autoUpgrades.rover.price;
-    if(cheese >= price) {
-    cheese -= price;
-    autoUpgrades.rover.quantity++;
-    autoUpgrades.rover.price += Math.floor(price * 0.10);
+  function buyPlasmaGun(){
+    let price = autoUpgrades.plasmaGun.price;
+    if(spaceRock >= price) {
+    spaceRock -= price;
+    autoUpgrades.plasmaGun.quantity++;
+    autoUpgrades.plasmaGun.price += Math.floor(price * 0.30);
     }else {
-    console.log("not enough cheese!")
+    console.log("not enough space rock!")
     }
-    document.getElementById("currentRover").innerText = autoUpgrades.rover.quantity.toString();
-    document.getElementById('roverPrice').innerText = autoUpgrades.rover.price.toString();
+    document.getElementById("currentPlasmaGun").innerText = autoUpgrades.plasmaGun.quantity.toString();
+    document.getElementById('plasmaGunPrice').innerText = autoUpgrades.rover.price.toString();
     update();
     }
 
-  function buyAlien(){
-   let price = autoUpgrades.alien.price;
-    if(cheese >= price) {
-    cheese -= price;
-    autoUpgrades.alien.quantity++;
-    autoUpgrades.alien.price += Math.floor(price * 0.10);
+  function buyExplosive(){
+   let price = autoUpgrades.explosive.price;
+    if(spaceRock >= price) {
+    spaceRock -= price;
+    autoUpgrades.explosive.quantity++;
+    autoUpgrades.explosive.price += Math.floor(price * 0.30);
     }else {
-    console.log("not enough cheese!")
+    console.log("not enough space rock!")
     }
-    document.getElementById("currentAlien").innerText = autoUpgrades.alien.quantity.toString();
-    document.getElementById('alienPrice').innerText = autoUpgrades.alien.price.toString();
+    document.getElementById("currentExplosive").innerText = autoUpgrades.explosive.quantity.toString();
+    document.getElementById('explosivePrice').innerText = autoUpgrades.explosive.price.toString();
     update();
      }
-
+ //  NOTE end of buyUpgrades section
 function update(){
-  document.getElementById("currentCheese").innerHTML = cheese.toString();
+  document.getElementById("currentSpaceRock").innerHTML = spaceRock.toString();
 }
+
+function collectAutoUpgrades() {
+  let autoSpaceRockTotal = 0;
+  for (let key in autoUpgrades) {
+    autoSpaceRockTotal += autoUpgrades[key].quantity * autoUpgrades[key].multiplier;
+  }
+  spaceRock += autoSpaceRockTotal;
+  document.getElementById("sps").innerText = autoSpaceRockTotal.toString();
+  update();
+}
+
+function startInterval(collectionInterval) {
+  collectionInterval = setInterval(collectAutoUpgrades, 3000);
+}
+
+startInterval()
+
+
